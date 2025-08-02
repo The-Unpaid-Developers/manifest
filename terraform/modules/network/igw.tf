777cf3ae718da8ea6e93ed5fc188bc25a353196c
Yue Zhen <smithquaz@gmail.com>
2025-08-02 14:03:41 +0800

    chore: remove tf

--- terraform/modules/network/igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "unpaid_developers_singapore_internet_gateway" {
-  vpc_id = aws_vpc.unpaid_developers_singapore_vpc.id
-
-  tags = {
-    Name = "unpaid-developers-singapore-internet-gateway"
-  }
-}
